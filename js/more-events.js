acfde77b6dd2a89eb0683d7a466c5690c4f39d13
NU-Faysol <nizamfaysol16@gmail.com>
2022-02-11 22:48:17 +0600

    added new code

--- js/more-events.js
+++ js/more-events.js
@@ -15,3 +15,31 @@ document
         document.body.style.backgroundColor = 'white';
 
   });
+              //4) keydown
+  // document.getElementById('delete-confirm').addEventListener('keydown', function(){
+
+  //   const deleteField = document.getElementById('delete-confirm');
+  //         console.log(deleteField.Value);
+  // });
+              //5) keypress
+  // document.getElementById('delete-confirm').addEventListener('keypress', function(){
+
+  //   const deleteField = document.getElementById('delete-confirm');
+  //         console.log(deleteField.Value);
+  // });
+
+
+            //6) keyup
+  // document.getElementById('delete-confirm').addEventListener('keyup', function(){
+
+  //   const deleteField = document.getElementById('delete-confirm');
+  //         console.log(deleteField.value);
+  // });
+
+          //7) change (যদি কিছু পরিবর্তন হয় তাহলে ধরে ফেলবে। তবে focus থেকে সরিয়ে নিলেই console এ পরিবর্তন দেখা যাবে।)
+
+  // document.getElementById("delete-confirm").addEventListener('change', function(){
+
+  //   const deleteField = document.getElementById("delete-confirm");
+  //         console.log(deleteField.value);
+  // })
